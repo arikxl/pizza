3a22cebcf3f459947a15478cb525505be6376915
Arik Alexandrov <arikxl@gmail.com>
2021-12-03 13:00:29 +0200

    animation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
-
 import './App.css';
+import Checkout from './components/Checkout';
 import AppHeader from './components/AppHeader';
 import CreatePizza from './components/CreatePizza';
-import Checkout from './components/Checkout';
+import cheese from './images/BaseCheese.png';
+import basil from './images/Basil.png';
+import mushroom from './images/Mushroom.png';
+import olive from './images/Olive.png';
+import pineapple from './images/Pineapple.png';
+import tomato from './images/Tomato.png';
 
 function App() {
+
+  const [ingredients, setIngredients] = useState([
+    {
+      img: cheese,
+      title: "Cheese",
+      isOn: false
+    },
+    {
+      img: basil,
+      title: "Basil",
+      isOn: false
+    },
+    {
+        img: mushroom,
+        title: "Mushroom",
+        isOn: false
+    },
+    {
+        img: olive,
+        title: "Olive",
+        isOn: false
+    },
+    {
+        img: pineapple,
+        title: "Pineapple",
+        isOn: false
+    },
+    {
+        img: tomato,
+        title: "Tomato",
+        isOn: false
+    },
+  ]);
+
   return (
     <div className="App">
       <AppHeader />
       <Routes>
-        <Route path="/" element={<CreatePizza />} />
+        <Route path="/" element={<CreatePizza
+          ingredients={ingredients}
+          setIngredients={setIngredients}/>} />
         <Route path="/checkout" element={<Checkout />} />
+
       </Routes>
     </div>
   );
--- src/components/CreatePizza.jsx
+++ src/components/CreatePizza.jsx
@@ -1,23 +1,72 @@
 import React from 'react';
+import { motion } from 'framer-motion/dist/es/index';
+
 
 import pizzaBase from '../images/PizzaBase.png';
-import cheese from '../images/BaseCheese.png';
-import basil from '../images/Basil.png';
-import mushroom from '../images/Mushroom.png';
-import olive from '../images/Olive.png';
-import pineapple from '../images/Pineapple.png';
-import tomato from '../images/Tomato.png';
 
-const CreatePizza = () => {
+
+const CreatePizza = ({ ingredients, setIngredients }) => {
+
+    const changeIngredients = (e, ingredient) => {
+        ingredient.isOn = e;
+        let newIngredients = JSON.parse(JSON.stringify(ingredients));
+        setIngredients(newIngredients)
+    }
+
     return (
         <main>
             <div className="image flex1">
-                <div className="flex1">
-                <img src={pizzaBase} alt="" />
+                <div className="pizza-container">
+                    {ingredients.map((ingredient, index) => (
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{
+                                y: ingredient.isOn ? 100 : -100,
+                                opacity: ingredient.isOn ? 1 : 0,
+                            }}
+                            transition={{ duration: 1 }}
+                            className="ingredients"
+                        >
+                            <img key={index} className="motion-ingredient"
+                                src={ingredient.img} alt={ingredient.title} />
+                        </motion.div>
+                    ))}
+                    <img src={pizzaBase} alt="pizza base" className="pizza" />
+
+                    {/* {ingredients.map((ingredient, index) => (
+                        <img className="ingredients" key={index}
+                            src={ingredient.img} alt={ingredient.title} />
+                    ))}
+                    <img src={pizzaBase} alt="pizza base" className="pizza" /> */}
 
                 </div>
             </div>
-            <div className="checkBox flex1">checkBox</div>
+            <div className="checkbox-container flex1">
+                {/* <input type="checkbox" name="cheese"
+                onChange={(e)=> changeIngredients(e.target.name)} />
+                <label htmlFor="cheese">Cheese</label> */}
+                {ingredients.map((ingredient, index) => (
+                    <label className="checkbox" key={index}>
+                        {ingredient.title}
+                        <input type="checkbox"
+                            checked={ingredients[ingredient]}
+                            onChange={(e) => {
+                                changeIngredients(e.currentTarget.checked, ingredient)
+                            }} />
+                        <span className="check-mark"></span>
+                    </label>
+                ))}
+
+                {/* <label className="checkBox">
+                    Cheese
+                    <input type="checkbox"
+                        checked={ingredients["cheese"]}
+                        onChange={(e) => {
+                            changeIngredients(e.currentTarget.checked, "cheese")
+                        }} />
+                    <span className="checkMark"></span>
+                </label> */}
+            </div>
         </main>
     )
 }
